ae04bc21afc2900946f0f20fd8610210643e8072
Ben Sim <crossfitjovi@gmail.com>
2022-12-12 03:55:47 +0800

    Update child component

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } fro
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css'],
   styles: ['h2 {color: green}'],
-  encapsulation: ViewEncapsulation.None
+  // encapsulation: ViewEncapsulation.None
 })
 export class ChildComponent implements OnInit {
   @Input() childMessage: string;
