c4ebe9792bb845aaee3cbf591e12e49a0e99f97c
Ben Sim <crossfitjovi@gmail.com>
2022-12-11 21:54:36 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+// export class AppComponent {
+//   title = 'my-first-angular';
+// }
+
+export class AppComponent implements OnInit {
   title = 'my-first-angular';
+  title2 = 'Hello World'
+  intervalSub;
+
+  getMin(a,b) {
+    if(a < b) {
+      return a;
+    }
+
+    return b;
+  }
+
+  ngOnInit() {
+    this.intervalSub = setInterval(() => {
+      console.log('Hello from ngOnInit');
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    if(this.intervalSub) {
+      clearInterval(this.intervalSub);
+    }
+  }
 }
